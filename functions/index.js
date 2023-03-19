326376263dd13dfbd41e20e6626ef26dc1c88362
minojiro <minojiro@users.noreply.github.com>
2023-03-19 19:07:20 +0900

    feat: やさしい言葉変換器爆誕

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const admin = require("firebase-admin");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "xxx",
 });
+const openai = new OpenAIApi(configuration);
+
+admin.initializeApp(functions.config().firebase);
+
+const askToChat = async (prompt) => {
+  const { data } = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    max_tokens: 100,
+    temperature: 0,
+  });
+  console.log(data.choices[0].text);
+  return data.choices[0].text;
+};
+
+exports.generateSafePost = functions.firestore
+  .document("unsafePosts/{docId}")
+  .onCreate(async (change, context) => {
+    const data = change.data();
+    const db = admin.firestore();
+    functions.logger.info(`start: ${data.text}`);
+    const text = await askToChat(
+      `以下の内容を優しい言い方に言い換えてください。\n${data.text}`
+    );
+    functions.logger.info(`generated: ${text}`);
+    await db.collection("posts").add({ ...data, text });
+    await db.doc(`unsafePosts/${context.params.docId}`).delete();
+    functions.logger.info(`done`);
+  });
